9d4d226190c70c6eff92bd5b663614d7a9cf1305
Lenny Maiorani <ldm5180@gmail.com>
2018-02-02 02:11:12 +0000

    Refactor using accumulate

--- include/01.hpp
+++ include/01.hpp
@@ -1,19 +1,20 @@
 #include <algorithm>
 #include <experimental/string_view>
+#include <numeric>
 namespace aoc {
 int captcha(std::experimental::string_view test) {
-  constexpr auto to_int = [](const auto pos) { return *pos - '0'; };
+  constexpr auto to_int = [](const auto pos) { return pos - '0'; };
   int accum = 0;
-  for (auto pos =
-           std::adjacent_find(std::cbegin(test), std::cend(test));
-       pos != std::cend(test);
-       pos = std::adjacent_find(std::next(pos), std::cend(test))) {
-    accum += to_int(pos);
+  if (test.empty()) {
+    return {};
   }
-
-  if (not test.empty() and test.front() == test.back()) {
-    accum += to_int(std::cbegin(test));
-  }
+  std::accumulate(std::begin(test), std::end(test), test.back(),
+                  [&](const auto cur, const auto next) {
+                    if (cur == next) {
+                      accum += to_int(cur);
+                    }
+                    return next;
+                  });
   return accum;
 }
 } // namespace aoc
